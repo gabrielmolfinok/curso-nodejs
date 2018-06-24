92fccfa52512e5c1adfc54b1c75706bab3258cd3
Gabriel <gabrielmolfinok@gmail.com>
2018-06-24 11:22:01 -0300

    Asyn y Await fix

--- 02-fundamentos/async-await.js
+++ 02-fundamentos/async-await.js
@@ -1,13 +1,11 @@
 /*** Asyn y Await ***/
 
-// Todo esto...
 let getNombre = async () => {
   return new Promise((resolve, reject) => {
     resolve('Gabriel');
   });
 }
 
-// Se resume en esto...
 let saludo = async () => {
 
   let nombre = await getNombre();
