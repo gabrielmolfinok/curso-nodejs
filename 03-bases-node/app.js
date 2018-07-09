5ba36d5ba5f3af781eec811d63486bfd94ee8f92
Gabriel <gabrielmolfinok@gmail.com>
2018-07-09 01:30:07 -0300

    Argumentos

---
+++ 03-bases-node/app.js
@@ -0,0 +1,29 @@
+
+const argv = require('yargs')
+                .command('listar', 'imprime en consola la tabla de multiplicar', {
+                  base: {
+                    demand: true,
+                    alias: 'b'
+                  },
+                  limite: {
+                    alias: 'l',
+                    default: 10
+                  }
+                })
+                .help()
+                .argv;
+
+
+const { crearArchivo } = require('./multiplicar/multiplicar');
+
+// console.log(process.argv);
+let argv2 = process.argv;
+// let parametro = argv[2];
+// let base = parametro.split('=')[1];
+
+console.log('Limite', argv.limite);
+
+
+// crearArchivo(base)
+//   .then(archivo => console.log(`Archivo creado: ${ archivo }`))
+//   .catch(e => console.log(e));
