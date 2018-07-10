9b05c55a968dfabbd4e26002c7b5495920c668ab
Gabriel Molfino <gabrielmolfinok@gmail.com>
2018-07-09 22:43:40 -0300

    s

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,29 +1,23 @@
+const argv = require('./config/yargs').argv;
+const colors = require('colors/safe');
 
-const argv = require('yargs')
-                .command('listar', 'imprime en consola la tabla de multiplicar', {
-                  base: {
-                    demand: true,
-                    alias: 'b'
-                  },
-                  limite: {
-                    alias: 'l',
-                    default: 10
-                  }
-                })
-                .help()
-                .argv;
+const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar');
 
+let comando = argv._[0];
 
-const { crearArchivo } = require('./multiplicar/multiplicar');
+switch (comando) {
 
-// console.log(process.argv);
-let argv2 = process.argv;
-// let parametro = argv[2];
-// let base = parametro.split('=')[1];
+  case 'listar':
+    listarTabla(argv.base, argv.limite);
+    break;
 
-console.log('Limite', argv.limite);
+  case 'crear':
+    crearArchivo(argv.base, argv.limite)
+      .then(archivo => console.log('Archivo creado:', colors.green(archivo)))
+      .catch(e => console.log(e));
+      break;
 
+  default:
+    console.log('Comando no reconocido');
 
-// crearArchivo(base)
-//   .then(archivo => console.log(`Archivo creado: ${ archivo }`))
-//   .catch(e => console.log(e));
+}
--- 03-bases-node/multiplicar/multiplicar.js
+++ 03-bases-node/multiplicar/multiplicar.js
@@ -1,7 +1,20 @@
 // Requireds
 const fs = require('fs');
+const colors = require('colors');
 
-let crearArchivo = (base) => {
+let listarTabla = (base, limite = 10) => {
+
+  console.log('==========================');
+  console.log(`Tabla del ${base}`.green);
+  console.log('==========================');
+
+  for (var i = 1; i <= limite; i++) {
+    console.log(`${base} * ${i} = ${base * i}`);
+  }
+
+}
+
+let crearArchivo = (base, limite = 10) => {
     return new Promise ((resolve, reject) => {
 
       if (!Number(base)) {
@@ -9,23 +22,24 @@ let crearArchivo = (base) => {
         return;
       }
 
-      let data = ''
+      let data = '';
 
-      for (var i = 1; i <= 10; i++) {
+      for (var i = 1; i <= limite; i++) {
         data += `${ base } * ${ i } = ${ base * i }\n`;
       }
 
 
-      fs.writeFile(`tablas/tabla-${base}.txt`, data, (err) => {
+      fs.writeFile(`tablas/tabla-del-${base}-al-${limite}.txt`, data, (err) => {
         if (err)
           reject(err)
         else
-          resolve(`tabla-${base}.txt`);
+          resolve(`tabla-del-${base}-al-${limite}.txt`);
       });
 
     });
 }
 
 module.exports = {
-  crearArchivo
+  crearArchivo,
+  listarTabla
 }
