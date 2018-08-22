e0c746760461100beff6d34c63eb8dacc7fd6d08
Gabriel <gabrielmolfinok@gmail.com>
2018-08-22 16:17:42 -0300

    - Handlebars 1st steps - Express 'routering' 1st steps

--- 06-webserver/server.js
+++ 06-webserver/server.js
@@ -1,18 +1,31 @@
 const express = require('express');
 const app = express();
+const hbs = require('hbs');
+
+require('./hbs/helpers');
+
+
 
 app.use( express.static(  __dirname + '/public' ) );
+
+// Express HBS engine
+hbs.registerPartials( __dirname + '/views/partials' );
+app.set('view engine', 'hbs');
  
-// app.get('/', (req, res) => {
+app.get('/', (req, res) => {
   
-//     let salida = {
-//         nombre: 'Gabriel',
-//         edad: 20,
-//         url: req.url
-//     }
+    res.render('home', {
+        nombre: 'Gabriel'
+    });
 
-//     res.send(salida);
+});
 
-// });
- 
+app.get('/about', (req, res) => {
+  
+    res.render('about');
+
+});
+
+// Listen genera el servidor y establece su puerto como primer parametro, y una funcion
+// En este caso podemos hacer un log para enterarnos si esta escuchando...
 app.listen(3000, () => console.log('Escuchando peticiones en el puerto 3000'));
