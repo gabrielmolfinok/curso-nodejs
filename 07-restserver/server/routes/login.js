959df7f2af584b77d05f7c8ad13a568a06ede47c
Gabriel Molfino <gabrielmolfinok@gmail.com>
2018-10-14 21:23:56 -0300

    Login small fix

--- 07-restserver/server/routes/login.js
+++ 07-restserver/server/routes/login.js
@@ -16,11 +16,7 @@ const Usuario = require('../models/usuario')
 app.post('/login', (req, res) => {
 
     let body = req.body
-
-    Usuario.find().exec((err, usuarios) => {
-        console.log(usuarios[0])
-    })
-
+    
     Usuario.findOne({ email: body.email }, (err, userDB) => {
 
         if (err) {
