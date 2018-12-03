35f101ab66e7ecd3fda2c76ac2af9b29e7ebf0f8
Gabriel Molfino <gabrielmolfinok@gmail.com>
2018-12-03 13:17:00 -0300

    Aplicacion de colas

--- 09-socket-colas/public/js/socket.nuevo-ticket.js
+++ 09-socket-colas/public/js/socket.nuevo-ticket.js
@@ -1,27 +1,32 @@
-
 // Comando para establecer la conexión
-var socket = io()
+var socket = io();
 
-var label = $('#lblNuevoTicket')
+var label = $('#lblNuevoTicket');
 
-socket.on('connect', () => {
 
-    console.log('Conectado al servidor')
+socket.on('connect', function() {
+    console.log('Conectado al servidor');
+});
 
-})
+socket.on('disconnect', function() {
+    console.log('Desconectado del servidor');
+});
 
-socket.on('disconnect', () => {
+// on 'estadoActual'
+socket.on('estadoActual', function(resp) {
 
-    console.log('Desconectado al servidor')
+    console.log(resp);
+    label.text(resp.actual);
 
-})
+});
+
 
 $('button').on('click', function() {
 
-    socket.emit('siguienteTicket', null, function(siguienteTicket){
+    socket.emit('siguienteTicket', null, function(siguienteTicket) {
 
-        label.text(siguienteTicket)
+        label.text(siguienteTicket);
 
-    })
+    });
 
-})
+});
